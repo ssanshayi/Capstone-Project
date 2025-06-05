1ba98f1b09761a208c7c22a0a859464332170dfd
YANG RUNDI <yrd1401138188@gmail.com>
2025-06-06 00:47:24 +0800

    modify the URL of API

--- components/uploadform.tsx
+++ components/uploadform.tsx
@@ -23,7 +23,7 @@ export default function UploadForm() {
     formData.append("file", file)
 
     try {
-      const response = await fetch("https://capstone-project-e9ct.onrender.com/predict", {
+      const response = await fetch("https://capstone-project2-x9ax.onrender.com", {
         method: "POST",
         body: formData,
       })
